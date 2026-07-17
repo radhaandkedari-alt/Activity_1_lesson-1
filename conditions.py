68efd3b2c950c8340d4112a1ee85f4c53df0420d
radhaandkedari-alt <radhaandkedari@gmail.com>
2026-07-17 10:15:07 +0400

    conditions

---
+++ conditions.py
@@ -0,0 +1,48 @@
+num=3
+if num>0:
+    print("The number is positive")
+num=-1
+if num>0:
+    print("The number1 is positive")
+
+    #assignment 2
+actual_cost=float(input("plese enter the acutal product price"))
+sale_amount=float(input("please enter the sales amount"))
+
+if sale_amount>actual_cost:
+    amount= sale_amount-actual_cost
+    print("the total profit", format(amount))
+else: 
+    print("no profit")
+    
+#assignment 3
+num1=int(input("please enter your first number"))
+if num1%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+
+#assignment 5
+name=input("please enter ur name")
+if name=="uma":
+    print("you are allowed to enter the school")
+else:
+    print("you are not allowed to enter the school")
+
+#assignment 6
+name=input("please enter your name")
+age=int(input("please enter your age"))
+if age>=16 and name=="uma":
+    print("you are allowed to enter the school")
+else:
+    print("you are not allowed to enter the school")
+
+
+
+
+
+
+
+
+
+
