fd5bab9ead306e75a9609485f460a40fe0ba38f3
radhaandkedari-alt <radhaandkedari@gmail.com>
2026-08-13 10:10:15 +0400

    return continue

---
+++ returncontinue.py
@@ -0,0 +1,45 @@
+a = input("Enter a word:")
+for i in a:
+    if i == 'a':
+        print("a is found")
+        break
+    else:
+        print("a is not found")
+
+#assignment 2
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print("Current variable value:", var)
+    
+#assignment 3
+for i in range(1,11):
+    if i%2==0:
+        continue
+    print(i)
+
+#assignment 4
+food = ["burger", "salad", "fries", "pizza", "rice"]
+for i in food:
+    if i == "fries" or i == "burger" or i == "pizza":
+        continue
+    print(i)
+
+#assignment 5
+def check_age(age):
+    if age < 18:
+        return "child"
+    else:
+        return "adult"
+age = int(input("enter your age:"))
+result = check_age(age)
+print(result)
+
+
+    
+    
+    
+
+
